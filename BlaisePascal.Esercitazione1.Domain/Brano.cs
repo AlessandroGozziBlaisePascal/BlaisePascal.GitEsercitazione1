163857f2cc513499bfb49e0f3bed59c40ec1640f
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2026-05-28 11:41:35 +0200

    added wpf

--- BlaisePascal.Esercitazione1.Domain/Brano.cs
+++ BlaisePascal.Esercitazione1.Domain/Brano.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,54 @@ namespace BlaisePascal.Esercitazione1.Domain
 {
     public class Brano
     {
-        public string Title { get; init; }
+        public string Title { get; private set; }
+        public Author Author { get; private set; }
+        public int DurationInSec { get; private set; }
+
+        public Brano(string title, Author author, int durationInSec)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+            if (author == null)
+                throw new ArgumentNullException(nameof(author), "Author cannot be null.");
+            if (durationInSec <= 0)
+                throw new ArgumentOutOfRangeException(nameof(durationInSec), "Duration must be greater than zero.");
+            Title = title;
+            Author = author;
+            DurationInSec = durationInSec;
+        }
+
+        public string GetTitle() => Title;
+        public Author GetAuthor() => Author;
+        public int GetDurationInSec() => DurationInSec;
+
+
+        public void SetTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+            Title = title;
+        }
+        public void SetAuthor(Author author)
+        {
+            if (author == null)
+                throw new ArgumentNullException(nameof(author), "Author cannot be null.");
+            Author = author;
+        }
+        public void SetDurationInSec(int durationInSec)
+        {
+            if (durationInSec <= 0)
+                throw new ArgumentOutOfRangeException(nameof(durationInSec), "Duration must be greater than zero.");
+            DurationInSec = durationInSec;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, Duration: {DurationInSec} seconds";
+        }
+
+
+        public bool ShortSong(int duration) => DurationInSec < duration;
     }
 }
