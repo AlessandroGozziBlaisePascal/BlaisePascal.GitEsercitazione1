1f9f13e43d03e0e5d9a42ea11eb23a17bbd8ee06
bryan.casali <bryan.casali.stud@ispascalcomandini.it>
2026-05-28 11:43:28 +0200

    Added Cd Class

--- BlaisePascal.Esercitazione1.Domain/Brano.cs
+++ BlaisePascal.Esercitazione1.Domain/Brano.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlaisePascal.Esercitazione1.Domain
+﻿namespace BlaisePascal.Esercitazione1.Domain
 {
     public class Brano
     {
---
+++ BlaisePascal.Esercitazione1.Domain/CD.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace BlaisePascal.Esercitazione1.Domain
+{
+    public class CD
+    {
+        public List<Brano> Brani { get; private set; }
+        
+        private string Titolo { get;  set; }
+        private string Autore { get;  set; }
+        public int DurataCd { get; private set; }
+        public CD(string titolo, string autore) 
+        { 
+            Brani = new List<Brano>();
+            Titolo = titolo;
+            Autore = autore;
+        }
+
+
+        public string GetTitolo() 
+        {
+            return Titolo;
+        }
+
+        public string GetAutore()
+        {
+            return Autore;
+        }
+
+        public void SetTitolo(string titolo) 
+        {
+            Titolo = titolo;
+        }
+
+        public void SetAutore(string autore) 
+        {
+            Autore = autore;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            
+            foreach (var brano in Brani)
+            {
+                sb.Append($"{brano.Title} \n");
+            }
+            return sb.ToString();
+        }
+
+        public int Durata() 
+        {
+            foreach(Brano b in Brani) 
+            {
+                DurataCd += b.DurationInSec;
+            }
+            return DurataCd;
+        }
+    }
+}
+
+
