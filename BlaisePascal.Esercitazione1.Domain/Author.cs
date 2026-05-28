8e346d5bbb29e126483bc4b251e4c440286e1871
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2026-05-28 11:43:11 +0200

    added class author

--- BlaisePascal.Esercitazione1.Domain/Author.cs
+++ BlaisePascal.Esercitazione1.Domain/Author.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace BlaisePascal.Esercitazione1.Domain
 {
-    internal class Author
+    public class Author
     {
+        public string ArtisticName { get; private set; }
+        public string Name { get; init; }
+        public string Surname { get; init; }
+
+        public Author(string artisticName, string name, string surname)
+        {
+            if (string.IsNullOrEmpty(artisticName))
+                throw new ArgumentException("Artistic name cannot be null or empty.", nameof(artisticName));
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            if (string.IsNullOrEmpty(surname))
+                throw new ArgumentException("Surname cannot be null or empty.", nameof(surname));
+
+            ArtisticName = artisticName;
+            Name = name;
+            Surname = surname;
+        }
+
+        public static Author Create(string artisticName, string name, string surname) => new Author(artisticName, name, surname);
+
+        public override string ToString()
+        {
+            return $"Author:{ArtisticName}, ({Name} {Surname})";
+        }
     }
 }
--- BlaisePascal.Esercitazione1.WPF/MainWindow.xaml.cs
+++ BlaisePascal.Esercitazione1.WPF/MainWindow.xaml.cs
@@ -16,9 +16,60 @@ namespace BlaisePascal.Esercitazione1.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
+            private CD mioCD;
+
+            public MainWindow()
+            {
+                InitializeComponent();
+                mioCD = new CD();
+            }
+
+            // Funzionalità Oggetto Brano
+            private void BtnAggiungiBrano_Click(object sender, RoutedEventArgs e)
+            {
+                if (!string.IsNullOrWhiteSpace(txtTitoloBrano.Text))
+                {
+                    Brano nuovoBrano = new Brano
+                    {
+                        Titolo = txtTitoloBrano.Text,
+                        Durata = txtDurataBrano.Text
+                    };
+
+                    mioCD.Brani.Add(nuovoBrano);
+                    AggiornaInterfaccia();
+
+                    txtTitoloBrano.Clear();
+                    txtDurataBrano.Clear();
+                    lblStatus.Text = $"Oggetto Brano '{nuovoBrano.Titolo}' creato e aggiunto.";
+                }
+            }
+
+            // Funzionalità Oggetto CD
+            private void BtnCreaCD_Click(object sender, RoutedEventArgs e)
+            {
+                mioCD.TitoloCD = txtTitoloCD.Text;
+                lblStatus.Text = $"CD rinominato in: {mioCD.TitoloCD}";
+            }
+
+            private void BtnTest_Click(object sender, RoutedEventArgs e)
+            {
+                if (mioCD.Brani.Count > 0)
+                {
+                    MessageBox.Show(mioCD.InfoCD, "Test Funzionalità CD");
+                }
+                else
+                {
+                    MessageBox.Show("Il CD è vuoto! Aggiungi dei brani prima.", "Errore Test");
+                }
+            }
+
+            private void AggiornaInterfaccia()
+            {
+                lstBraniCD.ItemsSource = null;
+                lstBraniCD.ItemsSource = mioCD.Brani;
+            }
         }
     }
 }
