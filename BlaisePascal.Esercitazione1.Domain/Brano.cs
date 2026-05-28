78b9868c1ba754dfae8a6c1cd450fb4208579171
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2026-05-28 11:16:35 +0200

    Primo commit

---
+++ BlaisePascal.Esercitazione1.Domain/Brano.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.Esercitazione1.Domain
+{
+    public class Brano
+    {
+        public string Title { get; init; }
+    }
+}
